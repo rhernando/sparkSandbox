b96a93ca62c8a6593c18b7d8cd5883ec9790f930
Ruben Hernando <rhernando@stratio.com>
2015-03-05 11:40:37 +0100

    dbchange

--- src/main/scala/StreamDriver.scala
+++ src/main/scala/StreamDriver.scala
@@ -22,11 +22,10 @@ object StreamDriver {
     ssc.checkpoint("tmp/")
 
     // Global information is available by using 1 as the WOEID.
-    val trends = twitterApi.getPlaceTrends(1).getTrends.map(trend => trend.getName)
+    // spain woeid => 23424950
+    val trends = twitterApi.getPlaceTrends(23424950).getTrends.map(trend => trend.getName)
     val stream = TwitterUtils.createStream(ssc, None, trends)
 
-    // get status with url
-    //stream.filter(status => status.getURLEntities().length > 0).map(status => status.getText).print()
     val urlTweets = stream.filter(status => status.getURLEntities().length > 0).persist()
 
     val updateCountFun = (values: Seq[Int], state: Option[Int]) => {
@@ -36,18 +35,17 @@ object StreamDriver {
       Some(currentCount + previousCount)
     }
 
-    val stateCounter = urlTweets.flatMap(status => status.getURLEntities.map(urlent => (urlent.getExpandedURL, 1 ))).updateStateByKey(updateCountFun)
+    // accumulate counter by url
+    val stateCounter = urlTweets.flatMap(status => status.getURLEntities.map(urlent => (urlent.getExpandedURL, 1))).updateStateByKey(updateCountFun)
     stateCounter.print()
 
 
-    val rowtrends = urlTweets.flatMap(tweet => tweet.getURLEntities.map(urlent => (urlent.getExpandedURL, tweet.getId, tweet.getUser.getId, tweet.getCreatedAt, tweet.getText)) )
-    //rowtrends.print()
+    val rowtrends = urlTweets.flatMap(
+      tweet => tweet.getURLEntities.map(
+        urlent => (urlent.getExpandedURL, tweet.getId, tweet.getUser.getId, tweet.getCreatedAt, tweet.getText, trends.filter(tt => tweet.getText.contains(tt)).toSet)
+      ))
 
-    rowtrends.saveToCassandra("twspark", "trendurls", SomeColumns("url", "tweet", "user_id", "produced", "trend"))
-
-
-    //val userRdd = ssc.cassandraTable("twspark", "users").select("twusername")
-    //userRdd.collect().foreach(println)
+    rowtrends.saveToCassandra("twspark", "trendurls", SomeColumns("url", "tweet", "user_id", "produced", "status", "trends"))
 
     ssc.start()
     ssc.awaitTermination()
